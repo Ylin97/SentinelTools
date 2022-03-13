fixed normalization when no sigma

bandplot.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,6 +15,8 @@ class BandFigure:
         """
         self.pixels    = band_pixels
         self.band_name = band_name
+        self.sigma     = 0
+        self.dolog     = False
 
     def plotall(self, issave=False, save_path=None, dpi=300)->plt.Figure:
         """
@@ -53,14 +56,15 @@ class BandFigure:
         plt.subplots_adjust(bottom=0.025)
         # plt.margins(0, 0)
         if issave:
-            if save_path:
-                if not os.path.exists(save_path):
-                    os.makedirs(save_path)
-                plt.savefig(os.path.join(save_path, f"{self.band_name}.png"), dpi=dpi)
-            else:
-                if not os.path.exists("Figure"):
-                    os.mkdir("Figure")
-                plt.savefig("Figure/{}.png".format(self.band_name), dpi=dpi)
+            self._save("all", save_path, dpi)
+            # if save_path:
+            #     if not os.path.exists(save_path):
+            #         os.makedirs(save_path)
+            #     plt.savefig(os.path.join(save_path, f"{self.band_name}_all.png"), dpi=dpi)
+            # else:
+            #     if not os.path.exists("Figure"):
+            #         os.mkdir("Figure")
+            #     plt.savefig("Figure/{}_all.png".format(self.band_name), dpi=dpi)
         return fig
 
     def plotfig(self, sigma=None, dolog=False, issave=False, 
@@ -77,50 +81,52 @@ class BandFigure:
         data_nl = self._norm_log(self.pixels, sigma, dolog)
         figname = self.band_name
         if sigma:
-            figname += f"_{sigma}sigma"
+            figname += f"_{sigma}sigma" 
         if dolog:
             figname += "_log"
         plt.imshow(data_nl, cmap=plt.cm.gray)
-        plt.title(figname)
+        plt.title(figname, fontweight='bold', fontsize=16)
         plt.axis("off")
         if issave:
-            if save_path:
-                if not os.path.exists(save_path):
-                    os.makedirs(save_path)
-                plt.savefig(os.path.join(save_path, figname + '.png'), dpi=dpi)
-            else:
-                if not os.path.exists("Figure"):
-                    os.mkdir("Figure")
-                plt.savefig("Figure/{}.png".format(figname), dpi=dpi)
+            self._save("single", save_path, dpi)
+        #     if save_path:
+        #         if not os.path.exists(save_path):
+        #             os.makedirs(save_path)
+        #         plt.savefig(os.path.join(save_path, figname + '.png'), dpi=dpi)
+        #     else:
+        #         if not os.path.exists("Figure"):
+        #             os.mkdir("Figure")
+        #         plt.savefig("Figure/{}.png".format(figname), dpi=dpi)
         return fig
 
     def _norm_log(self, band_data: np.ndarray, sigma=None, dolog=False) -> np.ndarray:
         """Normalization and log10"""
         # row, col = dataset.shape
+        self.sigma = sigma
         if dolog:
-            band_data = np.log10(band_data)
+            band_data  = np.log10(band_data)
+            self.dolog = True
         
         data_mean  = band_data.mean()
         data_std   = band_data.std()
         if not sigma:
             data_max = band_data.max()
-            data_min = band_data.min()
-            data_nl = band_data / (data_max - data_min)
+            data_nl = band_data / data_max
         elif sigma == 1:
             r_limit = data_mean + data_std
             l_limit = data_mean - data_std
-            data_nl = self._normalization(band_data, l_limit, r_limit)       
+            data_nl = self._norm_sigma(band_data, l_limit, r_limit)       
         elif sigma == 2:
             r_limit = data_mean + data_std * 2
             l_limit = data_mean - data_std * 2
-            data_nl = self._normalization(band_data, l_limit, r_limit)
+            data_nl = self._norm_sigma(band_data, l_limit, r_limit)
         elif sigma == 3:
             r_limit = data_mean + data_std * 3
             l_limit = data_mean - data_std * 3
-            data_nl = self._normalization(band_data, l_limit, r_limit)
+            data_nl = self._norm_sigma(band_data, l_limit, r_limit)
         return data_nl
 
-    def _normalization(self, band_data:np.ndarray, l_limit:float, r_limit:float):
+    def _norm_sigma(self, band_data:np.ndarray, l_limit:float, r_limit:float):
         """
         [l_limit, r_limit] -> [0, 1]
         Params:
@@ -140,6 +146,22 @@ class BandFigure:
                     data[i, j] = r_limit / distance
         return data
 
+    def _save(self, fig_t:str, save_path:str, dpi=300):
+        """Save figure to file"""
+        figname = self.band_name
+        if fig_t == "all":
+            figname += "_all"
+        elif fig_t == "single":
+            figname += "_norm" if not self.dolog else "_norm_log"
+        if save_path:
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            plt.savefig(os.path.join(save_path, figname + '.png'), dpi=dpi)
+        else:
+            if not os.path.exists("Figure"):
+                os.mkdir("Figure")
+            plt.savefig("Figure/{}.png".format(figname), dpi=dpi)
+
 
 if __name__ == "__main__":
     from bandreader import *
@@ -152,7 +174,14 @@ if __name__ == "__main__":
     band2 = Band(data_path, "Amplitude_VV")
     print(band2.byte_order)
     fig = BandFigure(band2.radar_pixels, "Amplitude_VV")
+
+    # img = fig.plotfig()
+    # img = fig.plotfig(sigma=2)
     # img = fig.plotfig(sigma=3, dolog=True)
+    # img = fig.plotfig(issave=True)
+    # img = fig.plotfig(dolog=True, issave=True)
+    # img = fig.plotfig(sigma=2, dolog=True, issave=True, save_path="Figure/single/1")
+    # img = fig.plotall()
     # img = fig.plotall(issave=True)
     img = fig.plotall(issave=True, save_path="Figure/all/1")
     plt.show()