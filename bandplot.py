a0951f373b3e26725d756826ae101764fc435dfc
Ylin97 <yalin20210411@outlook.com>
2022-09-05 13:13:43 +0800

    Add show() for BandFigure.

--- bandplot.py
+++ bandplot.py
@@ -89,6 +89,52 @@ class BandFigure:
             self._save("single", save_path, dpi)
         return fig
 
+    def show(*args, **kwargs):
+        """
+        Display all open figures.
+
+        Parameters
+        ----------
+        block : bool, optional
+            Whether to wait for all figures to be closed before returning.
+
+            If `True` block and run the GUI main loop until all figure windows
+            are closed.
+
+            If `False` ensure that all figure windows are displayed and return
+            immediately.  In this case, you are responsible for ensuring
+            that the event loop is running to have responsive figures.
+
+            Defaults to True in non-interactive mode and to False in interactive
+            mode (see `.pyplot.isinteractive`).
+
+        See Also
+        --------
+        ion : Enable interactive mode, which shows / updates the figure after
+            every plotting command, so that calling ``show()`` is not necessary.
+        ioff : Disable interactive mode.
+        savefig : Save the figure to an image file instead of showing it on screen.
+
+        Notes
+        -----
+        **Saving figures to file and showing a window at the same time**
+
+        If you want an image file as well as a user interface window, use
+        `.pyplot.savefig` before `.pyplot.show`. At the end of (a blocking)
+        ``show()`` the figure is closed and thus unregistered from pyplot. Calling
+        `.pyplot.savefig` afterwards would save a new and thus empty figure. This
+        limitation of command order does not apply if the show is non-blocking or
+        if you keep a reference to the figure and use `.Figure.savefig`.
+
+        **Auto-show in jupyter notebooks**
+
+        The jupyter backends (activated via ``%matplotlib inline``,
+        ``%matplotlib notebook``, or ``%matplotlib widget``), call ``show()`` at
+        the end of every cell by default. Thus, you usually don't have to call it
+        explicitly there.
+        """
+        plt.show(*args, **kwargs)
+
     def _norm_log(self, band_data: np.ndarray, sigma: int=None,
                   dolog: bool=False) -> np.ndarray:
         """Normalization and log10"""
@@ -178,4 +224,4 @@ if __name__ == "__main__":
     img = fig.plotall()
     # img = fig.plotall(issave=True)
     # img = fig.plotall(issave=True, save_path="Figure/all/2")
-    plt.show()
+    # plt.show()
